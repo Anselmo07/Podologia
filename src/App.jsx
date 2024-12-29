d15a1fd36480db702fd177f265ffa4f9020ae293
anselmo <anselmo.vecchio.jose@gmail.com>
2024-12-29 19:18:15 -0300

    Education, contact

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import NavBar from './components/navBar/navBar';
 import Home from './views/home/home';
 import About from './views/about/about';
+import Estudios from './views/estudios/estudios';
+import Contact from './views/contact/contact';
 
 function App() {
   
@@ -11,6 +13,8 @@ function App() {
       <NavBar />
       <Home/>
       <About/>
+      <Estudios/>
+      <Contact/>
     </div>
   )
 }
---
+++ src/views/estudios/estudios.jsx
@@ -0,0 +1,11 @@
+import "./estudios.css"
+
+const Estudios = () => {
+    return (
+        <div className="container-estudios">
+            <h2>ESTUDIOS</h2>
+        </div>
+    )
+}
+
+export default Estudios;
